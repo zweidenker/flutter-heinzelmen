3ddd38ad81c80f0a3d54dc1dcf91e396e349ea4e
Anton Borries <anton.borries@2denker.de>
2022-06-10 10:39:55 +0200

    Add script and first run to generate formal translation from poeditor

--- packages/apptive_grid_heinzelmen/lib/apptive_grid_heinzelmen.dart
+++ packages/apptive_grid_heinzelmen/lib/apptive_grid_heinzelmen.dart
@@ -1,5 +1,6 @@
 library apptive_grid_heinzelmen;
 
+export 'package:apptive_grid_heinzelmen/src/apptive_grid_user_management/formal_german/translation_de_for.dart';
 export 'package:apptive_grid_heinzelmen/src/attachment_image.dart';
 export 'package:apptive_grid_heinzelmen/src/configuration.dart';
 export 'package:apptive_grid_heinzelmen/src/stage_banner.dart';
