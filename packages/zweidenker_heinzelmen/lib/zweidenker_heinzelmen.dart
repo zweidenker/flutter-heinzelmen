ee56b8efd2818149cd4bc2e2097d070059743894
Anton Borries <anton.borries@2denker.de>
2023-01-24 12:12:24 +0100

    Fix File name

--- packages/zweidenker_heinzelmen/lib/zweidenker_heinzelmen.dart
+++ packages/zweidenker_heinzelmen/lib/zweidenker_heinzelmen.dart
@@ -2,5 +2,5 @@ library zweidenker_heinzelmen;
 
 export 'package:zweidenker_heinzelmen/src/app_version.dart';
 export 'package:zweidenker_heinzelmen/src/feature_toggle.dart';
+export 'package:zweidenker_heinzelmen/src/link_launcher.dart';
 export 'package:zweidenker_heinzelmen/src/simple_loader_mixin.dart';
-export 'package:zweidenker_heinzelmen/src/link_laucher.dart';
